17402af8a4b5ac60aa8e40fc24076525e104dd49
EvertonPI <evertonfran2410@gmail.com>
2021-08-31 00:48:25 -0300

    initial the project 3

---
+++ projeto3/weather/script.js
@@ -0,0 +1,6 @@
+// document.querySelector(".search").addEventListener("submit", e => {
+//   e.preventDefault();
+
+//   let input = document.querySelector("#searchInput").value;
+//   console.log(input);
+// });
